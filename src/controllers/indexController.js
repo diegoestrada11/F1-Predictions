44d92c2c27d4f49aeee790acbb7536e1cc3a198b
Diego Estrada <est21016@byui.edu>
2026-03-22 20:24:31 -0600

    added index controller and routes

---
+++ src/controllers/indexController.js
@@ -0,0 +1,3 @@
+export const buildHome = (req, res) => {
+  res.render("home");
+};
---
+++ src/routes/indexRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { buildHome } from "../controllers/indexController.js";
+
+const router = express.Router();
+
+router.get("/", buildHome);
+
+export default router;
